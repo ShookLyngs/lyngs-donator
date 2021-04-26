929f9fb81b35d3243c4e4da2c700daac8f9c0874
ShookLyngs <shook-lyngs@qq.com>
2021-04-26 17:56:04 +0800

    update(view/generate): fixed DraggableScrollableSheet big problem.

--- lib/pages/generate/followed_app_bar.dart
+++ lib/pages/generate/followed_app_bar.dart
@@ -3,10 +3,18 @@ import '../../main.dart';
 class FollowedAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return const SliverAppBar(
-      title: Text('title'),
+    return SliverAppBar(
+      title: FollowedAppBarTitle(),
       pinned: true,
     );
+  }
+}
+
+class FollowedAppBarTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    /*print(Get.mediaQuery.size.height);
+    print(Get.mediaQuery.padding.top);*/
+    return const Text('title');
   }
 }
--- lib/pages/generate/generate.dart
+++ lib/pages/generate/generate.dart
@@ -2,6 +2,7 @@ import '../../main.dart';
 import 'app_bar.dart';
 import 'followed_app_bar.dart';
 import 'followed_list.dart';
+import 'footer.dart';
 import 'full_balls_list.dart';
 
 class GeneratePage extends StatelessWidget {
@@ -9,41 +10,45 @@ class GeneratePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // ignore: lines_longer_than_80_chars
-    print(kToolbarHeight+ Get.statusBarHeight);
+    var windowHeight = Get.height;
+    var statusBarHeight = Get.statusBarHeight / Get.pixelRatio;
+    var minHeight = (kToolbarHeight + statusBarHeight) / Get.height;
 
-    return Container(
-      child: Stack(
-        children: [
-          CustomScrollView(
-            physics: const BouncingScrollPhysics(),
-            slivers: [
-              GenerateAppBar(),
-              GenerateFullBallsList(),
-              GenerateFollowedList(),
-            ],
-          ),
-          DraggableScrollableSheet(
-            initialChildSize: 132.4 / Get.height,
-            minChildSize: 132.4 / Get.height,
+    return Stack(
+      children: [
+        CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            GenerateAppBar(),
+            GenerateFullBallsList(),
+            GenerateFollowedList(),
+          ],
+        ),
+        Container(
+          height: windowHeight,
+          child: DraggableScrollableSheet(
+            initialChildSize: minHeight,
+            minChildSize: minHeight,
             maxChildSize: 1,
             builder: (_context, scrollController) {
-              return Container(
-                color: Colors.white,
-                child: CustomScrollView(
-                  controller: scrollController,
-                  physics: const BouncingScrollPhysics(),
-                  slivers: [
-                    FollowedAppBar(),
-                    GenerateFullBallsList(),
-                    GenerateFollowedList(),
-                  ],
-                ),
-              );
+              return LayoutBuilder(builder: (_boxContext, _boxConstraints) {
+                return Container(
+                  color: Colors.white,
+                  child: CustomScrollView(
+                    controller: scrollController,
+                    physics: const BouncingScrollPhysics(),
+                    slivers: [
+                      FollowedAppBar(),
+                      GenerateFullBallsList(),
+                      GenerateFollowedList(),
+                    ],
+                  ),
+                );
+              });
             },
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
