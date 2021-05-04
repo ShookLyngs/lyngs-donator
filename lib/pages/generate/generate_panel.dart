127bcba779214f8cb8a8f1fc9e75ea00aa4ea2eb
ShookLyngs <shook-lyngs@qq.com>
2021-05-04 18:38:50 +0800

    update(generate_panel): update header background. update list content.

--- lib/pages/generate/generate_panel.dart
+++ lib/pages/generate/generate_panel.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 
 import '../../main.dart';
@@ -18,6 +20,7 @@ class GeneratePanel extends GetxWidget<GeneratePageState> {
         color: Theme.of(context).cardColor,
         child: CustomScrollView(
           controller: scrollController,
+          physics: const BouncingScrollPhysics(),
           slivers: [
             SliverStickyHeader(
               header: PanelHeader(),
@@ -30,23 +33,73 @@ class GeneratePanel extends GetxWidget<GeneratePageState> {
   }
 }
 
-class PanelHeader extends StatelessWidget {
+class PanelHeader extends GetxWidget<GeneratePageState> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 80,
-      child: TextButton(
-        onPressed: () {  },
-        child: const Text('我的关注'),
+    return ClipRect(
+      child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+          child: Container(
+            height: 70,
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor.withAlpha(160),
+            ),
+            child: Row(
+              children: [
+                const SizedBox(width: 16),
+                TextButton(
+                  onPressed: () {
+                    state.panelController.close();
+                  },
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(state.innerBorderRadius),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.expand_more,
+                        size: 32,
+                      ),
+                      const SizedBox(width: 6),
+                      Text(
+                        '我的关注',
+                        style: Theme.of(context).textTheme.headline1!.copyWith(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      const SizedBox(width: 12),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(width: 16),
+              ],
+            ),
+          ),
       ),
     );
   }
 }
 
-class PanelBody extends StatelessWidget {
+class PanelBody extends GetxWidget<GeneratePageState> {
   @override
   Widget build(BuildContext context) {
     return SliverList(delegate: SliverChildListDelegate([
+      Padding(
+        padding: const EdgeInsets.all(16),
+        child: Container(
+          height: 100,
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColorLight,
+            borderRadius: BorderRadius.all(state.innerBorderRadius),
+          ),
+          child: const Center(child: Text('card')),
+        ),
+      ),
       const ListTile(
         title: Text('Title'),
         subtitle: Text('SubTitle'),
