e1815f4a8c30e5881b3766fcea3548d9059971db
ShookLyngs <shook-lyngs@qq.com>
2021-05-19 01:51:16 +0800

    update(view/home): updated WatchingList widget styles.

--- lib/pages/home/watching_list.dart
+++ lib/pages/home/watching_list.dart
@@ -18,9 +18,7 @@ class WatchingList extends StatelessWidget {
           const SizedBox(height: 8),
 
           WatchingListItem(),
-          const Divider(),
           WatchingListItem(),
-          const Divider(),
           WatchingListItem(),
         ],
       ),
@@ -33,87 +31,90 @@ class WatchingListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 6),
-      child: ClipRRect(
-        borderRadius: const BorderRadius.all(Radius.circular(12)),
-        child: Ink(
+      child: Ink(
+        decoration: BoxDecoration(
           color: Theme.of(context).cardColor,
-          child: InkWell(
-            onTap: () {},
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Row(children: [
-                          Text(
-                            '#21051',
-                            style: Theme.of(context).textTheme.headline5!.copyWith(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ]),
-                      ),
-                      Icon(
-                        Icons.watch_later_outlined,
-                        size: 16,
-                        color: Theme.of(context).textTheme.caption!.color,
-                      ),
-                      const SizedBox(width: 4),
-                      const Text('5月19日 周三'),
-                    ],
-                  ),
-
-                  const SizedBox(height: 2),
-
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: SizedBox(
-                          height: 24,
-                          child: ListView(
-                            scrollDirection: Axis.horizontal,
-                            shrinkWrap: true,
-                            physics: const BouncingScrollPhysics(),
-                            children: [
-                              WatchingListNumber('12'),
-                              WatchingListNumber('12'),
-                              WatchingListNumber('12'),
-                              WatchingListNumber('12'),
-                              WatchingListNumber('12'),
-
-                              WatchingListNumber('/'),
-
-                              WatchingListNumber('12'),
-                              WatchingListNumber('12'),
-                            ],
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 12),
-                      Container(
-                        width: 8,
-                        height: 8,
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).primaryColor,
-                          borderRadius: const BorderRadius.all(Radius.circular(8)),
-                        ),
-                      ),
-                      const SizedBox(width: 8),
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+        ),
+        child: InkWell(
+          onTap: () {},
+          customBorder: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(children: [
+                  Expanded(
+                    child: Row(children: [
                       Text(
-                        '4倍 / 8CNY',
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColorDark,
+                        '#21051',
+                        style: Theme.of(context).textTheme.headline5!.copyWith(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ],
+                    ]),
                   ),
-                ],
-              ),
+                  Icon(
+                    Icons.watch_later_outlined,
+                    size: 16,
+                    color: Theme.of(context).textTheme.caption!.color,
+                  ),
+                  const SizedBox(width: 4),
+                  const Text('5月19日 周三'),
+                ]),
+
+                const SizedBox(height: 2),
+
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: 24,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          physics: const BouncingScrollPhysics(),
+                          children: [
+                            WatchingListNumber('12'),
+                            WatchingListNumber('12'),
+                            WatchingListNumber('12'),
+                            WatchingListNumber('12'),
+                            WatchingListNumber('12'),
+
+                            WatchingListNumber('/'),
+
+                            WatchingListNumber('12'),
+                            WatchingListNumber('12'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 12),
+                    Container(
+                      width: 8,
+                      height: 8,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(8),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      '4倍 / 8CNY',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColorDark,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
